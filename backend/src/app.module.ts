9723ce7fee1d96d8f3d073978fed5d6ff6a0cd5a
yukina1418 <yukina1418@gmail.com>
2022-04-21 23:43:14 +0900

    로그아웃 구현

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
@@ -15,6 +15,8 @@ import { ProductModule } from './apis/product/product.module';
 import { ProductInfoModule } from './apis/productinfo/productinfo.module';
 import { SubscriptionModule } from './apis/subscription/subscription.module';
 import { UserModule } from './apis/User/user.module';
+import type { RedisClientOptions } from 'redis';
+import * as redisStore from 'cache-manager-redis-store';
 
 //하위 apis의 파일 각각에서 만든 api가 해당하는 폴더의 module에 합쳐지고
 // 그것을 app.module에서 호출해서 main단에서 사용한다
@@ -40,16 +42,21 @@ import { UserModule } from './apis/User/user.module';
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'my-database',
       port: 3306,
       username: 'root',
-      password: '5515',
-      database: 'mainproject',
+      password: 'root',
+      database: 'mydocker02',
       entities: [__dirname + '/apis/**/*.entity.*'],
       synchronize: true,
       logging: true,
       retryAttempts: 30,
       retryDelay: 5000,
+    }),
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      url: 'redis://my-redis:6379',
+      isGlobal: true,
     }),
   ],
   // controllers: [AppController],
--- backend/src/commons/auth/jwt-refresh.strategy.ts
+++ backend/src/commons/auth/jwt-refresh.strategy.ts
@@ -1,18 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-jwt';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER, Inject } from '@nestjs/common';
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'refresh') {
-  constructor() {
+  constructor(
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
+  ) {
     super({
       //
       jwtFromRequest: (req) => req.headers.cookie.replace('refreshToken=', ''),
       secretOrKey: 'myRefreshKey',
+      passReqToCallback: true,
     }); // 여기는 토큰 비밀번호
   } // 검증부
 
-  validate(payload) {
+  async validate(req, payload) {
+    const rawHeadersInRefreshToken = req.rawHeaders
+      .filter((ele) => {
+        return ele.match(/refresh/);
+      })[0]
+      .split('=')[1];
+
+    const check = await this.cacheManager.get(rawHeadersInRefreshToken);
+
+    if (check) throw new UnauthorizedException();
+
     return {
       login_id: payload.login_id,
     };
